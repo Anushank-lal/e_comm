ea9b8667614ff5482f09964dea12ae2773e6bf68
anushank-lal <anushanklal@gmail.com>
2015-12-23 00:07:53 +0530

    validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ActiveRecord::Base
+  validates :firstname, :lastname, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { in: 7..20 }
+
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :name, :price, :description, :status, presence: true
+  enum status: { enabled: 0, disabled: 1 }
+  validates_inclusion_of :status, :in => ['enabled', 'disabled']
 end
