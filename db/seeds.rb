59db3e12dc9b532c118f9d57819001ffe5ef9c8b
anushank-lal <anushanklal@gmail.com>
2015-12-23 21:02:35 +0530

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+Product.create!(status:0 , name: "Product 1", price: 10.40, description: "Test")
+Product.create!(status:0 , name: "Product 2", price: 20.10, description: "Test description")
+Product.create!(status:0 , name: "Product 3", price: 25.20, description: "Test description")
+Product.create!(status:0 , name: "Product 4", price: 21.10, description: "Test description")
+Product.create!(status:0 , name: "Product 5", price: 22.50, description: "Test description")
+
+Customer.create!(firstname: "Anushank", lastname: "lal", email: "anushank@gmail.com", password: "1234567")
+Customer.create!(firstname: "Demo", lastname: "demo", email: "demo@gmail.com", password: "qwerty1")
+Customer.create!(firstname: "Test", lastname: "test", email: "test@gmail.com", password: "qwerty2")
+
