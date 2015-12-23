f5288a11930fd41839d065e942de659f5827ed2d
anushank-lal <anushanklal@gmail.com>
2015-12-23 22:21:34 +0530

    oder

--- app/api/e_comm/v1/orders.rb
+++ app/api/e_comm/v1/orders.rb
@@ -7,27 +7,31 @@ module EComm
 
       desc "Add Product to Cart items"
       params do
-        requires :order_id, type: Integer, allow_blank: false
+        requires :customer_id, type: Integer, allow_blank: false
         requires :product_id, type: Integer, allow_blank: false
         requires :qty, type: Integer, allow_blank: false
       end
 
-      put :cart_items, rabl: "/api/v1/orders/cart_items.json.rabl" do
-        begin
-          @cart_items = OrderLine.new
+      put :cart_items, rabl: "/api/v1/orders/cart_item.json.rabl" do
+        #begin
           product = Product.find_by(id: params[:product_id])
           unit_price = product.price
           total_price = (unit_price * params[:qty])
-          @cart_items.attributes =
+          order_no = rand.to_s[2..8]
+          @order = Order.find_by(customer_id: params[:customer_id])
+          @order = Order.create!(order_no: order_no, customer_id: params[:customer_id], total: total_price, date: Date.today) if @order.blank?
+
+          @cart_item = OrderLine.new
+          @cart_item.attributes =
           {
-            order_id: params[:order_id], product_id: params[:product_id],
+            order_id: @order.id, product_id: params[:product_id],
             qty: params[:qty], unit_price: unit_price, total_price: total_price
           }
 
-          error!({error: @cart_items.errors.full_messages}, 400) if !(@cart_items.save!)
-        rescue Exception => e
-          error!({ error: "Internal Server Error" }, 500)
-        end
+          error!({error: @cart_item.errors.full_messages}, 400) if !(@cart_item.save!)
+        # rescue Exception => e
+        #   error!({ error: "Internal Server Error" }, 500)
+        # end
       end
 
     end
