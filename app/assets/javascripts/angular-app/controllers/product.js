fc50cf0854ecec692b13c3443486047d3987468a
Sanyam Jain <sanyamjain2003@gmail.com>
2015-12-23 23:53:12 +0530

    ADD EDIT PRODUCT

--- app/assets/javascripts/angular-app/controllers/product.js
+++ app/assets/javascripts/angular-app/controllers/product.js
@@ -1,10 +1,60 @@
 'use strict';
 
 /* Controllers */
-ecom.controller("ProductController", ['$scope', 'ProductList',
-  function($scope, ProductList) {
+ecom.controller("ProductsController", ['$scope', 'productList',
+  function($scope, productList) {
+    $scope.productList = new productList();
+  }
+]);
 
-    $scope.productList = new ProductList();
+/* Controllers */
+ecom.controller("ProductController", ['$scope', 'productService',
+  function($scope, productService) {
+
+    $scope.product = {
+      id: '',
+      name: '',
+      description: '',
+      status: false,
+      price: ''
+    };
+
+    $scope.addProduct = function () {
+      if ($scope.newProduct.$valid) {
+        $scope.isSaving = true;
+        var promise = productService.login($scope.product);
+        promise.then(
+          function(response){ // success
+            $location.path("/");
+          },
+          function(error){ // error
+            $scope.newProductErrorMessage = error;
+          }
+        ).finally(function() {
+          // Always execute this on both error and success
+          $scope.isSaving = false;
+        });
+      };
+    };
+
+
+    $scope.updateProduct = function () {
+      if ($scope.editProduct.$valid) {
+        $scope.isSaving = true;
+        var promise = productService.login($scope.product);
+        promise.then(
+          function(response){ // success
+            $location.path("/");
+          },
+          function(error){ // error
+            $scope.editProductErrorMessage = error;
+          }
+        ).finally(function() {
+          // Always execute this on both error and success
+          $scope.isSaving = false;
+        });
+      };
+    };
 
   }
 ]);
