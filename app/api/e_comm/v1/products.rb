8aefb7ed23b71f1f5fd62f2d105271f62ce51da3
anushank-lal <anushanklal@gmail.com>
2015-12-24 00:22:51 +0530

    enable exception handling

--- app/api/e_comm/v1/products.rb
+++ app/api/e_comm/v1/products.rb
@@ -11,12 +11,12 @@ module EComm
       end
 
       get "products/:id", rabl: "/api/v1/products/product.json.rabl" do
-        #begin
+        begin
           @product = Product.find_by(id: params[:id])
           error!({error: ('No product available with this id.')}, 400) if @product.nil?
-        # rescue Exception => e
-        #   error!({ error: I18n.t('api.internal_server_error')}, 500)
-        # end
+        rescue Exception => e
+          error!({ error: I18n.t('api.internal_server_error')}, 500)
+        end
       end
 
 
@@ -27,14 +27,14 @@ module EComm
       end
 
       get :products, rabl: "/api/v1/products/products.json.rabl" do
-        #begin
+        begin
           limit = params[:limit].nil? ? 10 : params[:limit]
           offset = params[:offset].nil? ? 0 : params[:offset]
           @products = Product.limit(limit).offset(offset)
           error!({error: ('No products available.')}, 400) if @products.nil?
-        # rescue Exception => e
-        #   error!({ error: I18n.t('api.internal_server_error')}, 500)
-        # end
+        rescue Exception => e
+          error!({ error: I18n.t('api.internal_server_error')}, 500)
+        end
       end
 
       desc "Create new Product"
@@ -46,7 +46,7 @@ module EComm
       end
 
       post :products, rabl: "/api/v1/products/create.json.rabl" do
-        #begin
+        begin
           product = Product.find_by(name: params[:name])
           error!({error: ("Product name already exists")}, 400) if product.present?
 
@@ -59,9 +59,9 @@ module EComm
           }
 
           error!({error: @product.errors.full_messages}, 400) if !(@product.save!)
-        # rescue Exception => e
-        #   error!({ error: ('Internal Server Error')}, 500)
-        # end
+        rescue Exception => e
+          error!({ error: ('Internal Server Error')}, 500)
+        end
       end
 
       desc "Edit a Product"
@@ -74,7 +74,7 @@ module EComm
       end
 
       patch "products/:id", rabl: "/api/v1/products/create.json.rabl" do
-        #begin
+        begin
           @product = Product.find_by(id: params[:id])
           error!({error: ("Product name not exists")}, 400) if @product.nil?
           status = params[:status] == true ? 'enabled' : 'disabled'
@@ -83,9 +83,9 @@ module EComm
           if !@product.errors.blank?
             error!({error: @product.errors.full_messages}, 400)
           end
-        # rescue Exception => e
-        #   error!({ error: ('Internal Server Error')}, 500)
-        # end
+        rescue Exception => e
+          error!({ error: ('Internal Server Error')}, 500)
+        end
       end
 
 
